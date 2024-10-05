b1bbe27db25c862bdf0feb4d1ae38d05c83b22e3
Henrique Santana <43079979+henriquesantanati@users.noreply.github.com>
2024-10-05 19:57:31 +0100

    Added option to transfer and the overloading for the constructor using this()

--- BankAccount.java
+++ BankAccount.java
@@ -8,20 +8,33 @@ public class BankAccount {
         this.balance = initialBalance;
     }
 
+    // Construtor overloading for accounts without initialBalance parameter
+    public BankAccount(String accountNumber){
+        this(accountNumber, 0.0); // this() here will call the above constructor
+    }
+
     public void deposit(double amount){
-        balance += amount;
+        this.balance += amount;
     }
 
     public void withdraw(double amount){
-        if (amount <= balance){
-            balance -= amount;
+        if (amount <= this.balance){
+            this.balance -= amount;
         } else {
             System.out.println("Insufficient funds");
         }
     }
 
     public double getBalance(){
-        return balance;
+        return this.balance;
     }
 
+    public void transfer(BankAccount recipient, double amount){
+        if (amount <= this.balance){
+            this.withdraw(amount);
+            recipient.deposit(amount);
+        } else {
+            System.out.println("Insufficient funds for transfer");
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
     public static void main(String[] args){
-        BankAccount account = new BankAccount("123456", 1000);
-        account.deposit(500);
-        account.withdraw(200);
-        System.out.println("Current balance: " + account.getBalance());
+        BankAccount account1 = new BankAccount("123456", 1000);
+        BankAccount account2 = new BankAccount("789012");
+
+        account1.deposit(500);
+        account1.withdraw(200);
+        System.out.println("Current balance: " + account1.getBalance());
+
+        account1.transfer(account2, 300.0);
+        System.out.println("Account 1 balance after transfer: " + account1.getBalance());
+        System.out.println("Account 2 balance after transfer: " + account2.getBalance());
+
     }
 }
