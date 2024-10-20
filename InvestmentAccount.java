4f1975175e3f48d851eb2b32e239b644eba315a0
Henrique Santana <43079979+henriquesantanati@users.noreply.github.com>
2024-10-20 15:07:34 +0100

    Adding this and super usage

--- InvestmentAccount.java
+++ InvestmentAccount.java
@@ -1,6 +1,10 @@
 public class InvestmentAccount extends BankAccount {
     private double interestRate;
 
+    public InvestmentAccount(String accountNumber, double initialBalance) {
+        this(accountNumber, initialBalance, 0.005); // Default interest rate of 0.5%
+    }
+
     public InvestmentAccount(String accountNumber, double initialBalance, double interestRate) {
         super(accountNumber, initialBalance, AccountType.INVESTMENT);
         this.interestRate = interestRate;
@@ -8,6 +12,12 @@ public class InvestmentAccount extends BankAccount {
 
     public void applyInterest() {
         double interest = getBalance() * interestRate;
-        deposit(interest);
+        super.deposit(interest);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        System.out.println("Special handling for investment account deposit");
+        super.deposit(amount); 
     }
 }
